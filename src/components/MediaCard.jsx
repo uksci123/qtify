e76237ee56cfdf13cb3bc099010716a2cdc22dc1
Uksci123 <ukscihub123@gmail.com>
2023-09-28 20:36:32 +0530

    some-change-grid-title-added

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -1,14 +1,17 @@
 export default function MediaCard({id , image  , title , follows , isVisible} ) {
   return (
-    <div className={`${isVisible ? 'block' : 'hidden'} w-36 rounded-xl m-2 bg-white`}>
-      <img
-        className='w-full h-44 rounded-t-xl'
-        src={image}
-        title={title}
-      />
-      <div className='w-24 bg-black text-white text-xs ml-2 my-2 rounded-lg'>
-        <button className='ml-2 p-1'>{(follows/1000).toFixed(1)}K Follows</button>
+    <div className={`${isVisible ? 'block' : 'hidden'}`}>
+      <div className=" w-36 rounded-xl m-2 bg-white pb-1">
+        <img
+          className='w-full h-44 rounded-t-xl'
+          src={image}
+          title={title}
+        />
+        <div className='w-24 bg-black text-white text-xs ml-2 my-2 rounded-lg'>
+          <button className='ml-2 p-1'>{(follows/1000).toFixed(1)}K Follows</button>
+        </div>
       </div>
+      <p className="text-xs ml-2">{title}</p>
     </div>
   );
 }
